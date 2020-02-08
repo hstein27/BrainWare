7344a338df481ec35438e3e4e878d7dd8439c74e
Harris Stein <hstein27@gmail.com>
2020-02-08 13:39:06 -0500

    Refactor into one SQL call

--- Web/Infrastructure/OrderService.cs
+++ Web/Infrastructure/OrderService.cs
@@ -16,70 +16,58 @@ namespace Web.Infrastructure
             using (var database = new Database())
             {
                 // Get the orders
-                var sql1 =
-                    "SELECT c.name, o.description, o.order_id FROM company c INNER JOIN [order] o on c.company_id=o.company_id";
+                //HS - get all order info in one query
+                var sql = @"SELECT c.name, o.description, o.order_id, op.price,
+                            op.product_id, op.quantity, p.name, p.price 
+                            FROM company c INNER JOIN[order] o on c.company_id = o.company_id
+                            inner join [orderproduct] op on o.order_id = op.order_id
+                            INNER JOIN [product] p on op.product_id = p.product_id";
 
                 //HS - move lists out of new Using statements so they are accessible later. Rename them for clarity.
-                var orders = new List<Order>();
+                var orders = new Dictionary<int, Order>();
                 var orderProducts = new List<OrderProduct>();
                 //HS - Using statement will take care of  disposing
-                using (var reader1 = database.ExecuteReader(sql1))
+                using (var reader1 = database.ExecuteReader(sql))
                 {
                     while (reader1.Read())
                     {
-                        var record1 = (IDataRecord)reader1;
-
-                        orders.Add(new Order()
+                        var rec = (IDataRecord)reader1;
+                        int orderId = rec.GetInt32(2);
+                        Order order = null;
+                        if (!orders.ContainsKey(orderId))
                         {
-                            CompanyName = record1.GetString(0),
-                            Description = record1.GetString(1),
-                            OrderId = record1.GetInt32(2),
-                            OrderProducts = new List<OrderProduct>()
-                        });
-
+                            order = new Order()
+                            {
+                                CompanyName = rec.GetString(0),
+                                Description = rec.GetString(1),
+                                OrderId = orderId,
+                                OrderProducts = new List<OrderProduct>()
+                            };
+                            orders.Add(orderId, order);
+                        }
+                        else
+                        {
+                            order = orders[orderId];
+                        }
+                        //add product to OrderProducts list
+                        var orderProduct = new OrderProduct()
+                        {
+                            OrderId = orderId,
+                            Price = rec.GetDecimal(3),
+                            ProductId = rec.GetInt32(4),
+                            Quantity = rec.GetInt32(5),
+                            Product = new Product()
+                            {
+                                Name = rec.GetString(6),
+                                Price = rec.GetDecimal(7)
+                            }
+                        };
+                        order.OrderProducts.Add(orderProduct);
+                        order.OrderTotal += (orderProduct.Price * orderProduct.Quantity);
                     }
                     reader1.Close();
                 }
-
-                //Get the order products
-                var sql2 =
-                    "SELECT op.price, op.order_id, op.product_id, op.quantity, p.name, p.price FROM orderproduct op INNER JOIN product p on op.product_id=p.product_id";
-
-                //HS - Using statement will take care of  disposing
-                using (var reader2 = database.ExecuteReader(sql2))
-                {
-                    while (reader2.Read())
-                    {
-                        var record2 = (IDataRecord)reader2;
-
-                        orderProducts.Add(new OrderProduct()
-                        {
-                            OrderId = record2.GetInt32(1),
-                            ProductId = record2.GetInt32(2),
-                            Price = record2.GetDecimal(0),
-                            Quantity = record2.GetInt32(3),
-                            Product = new Product()
-                            {
-                                Name = record2.GetString(4),
-                                Price = record2.GetDecimal(5)
-                            }
-                        });
-                    }
-
-                    reader2.Close();
-                }
-
-                foreach (var order in orders)
-                {
-                    //use LINQ to find exact matches instead of looping whole List
-                    foreach (var orderproduct in orderProducts.Where(op => op.OrderId == order.OrderId))
-                    {
-                        order.OrderProducts.Add(orderproduct);
-                        order.OrderTotal = order.OrderTotal + (orderproduct.Price * orderproduct.Quantity);
-                    }
-                }
-
-                return orders;
+                return orders.Values.ToList();
             }
         }
     }
