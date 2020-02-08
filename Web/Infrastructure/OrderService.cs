5bedfbf9f9b03effee0c44b6ac876f0c58413a22
Harris Stein <hstein27@gmail.com>
2020-02-08 12:54:32 -0500

    Refactor Order class 1st pass

--- Web/Infrastructure/OrderService.cs
+++ Web/Infrastructure/OrderService.cs
@@ -19,61 +19,61 @@ namespace Web.Infrastructure
             var sql1 =
                 "SELECT c.name, o.description, o.order_id FROM company c INNER JOIN [order] o on c.company_id=o.company_id";
 
-            var reader1 = database.ExecuteReader(sql1);
-
+            //HS - move lists out of new Using statements so they are accessible later
             var values = new List<Order>();
-            
-            while (reader1.Read())
-            {
-                var record1 = (IDataRecord) reader1;
-
-                values.Add(new Order()
+            var values2 = new List<OrderProduct>();
+            //HS - Using statement will take care of  disposing
+            using (var reader1 = database.ExecuteReader(sql1))
+            {           
+                while (reader1.Read())
                 {
-                    CompanyName = record1.GetString(0),
-                    Description = record1.GetString(1),
-                    OrderId = record1.GetInt32(2),
-                    OrderProducts = new List<OrderProduct>()
-                });
+                    var record1 = (IDataRecord)reader1;
 
+                    values.Add(new Order()
+                    {
+                        CompanyName = record1.GetString(0),
+                        Description = record1.GetString(1),
+                        OrderId = record1.GetInt32(2),
+                        OrderProducts = new List<OrderProduct>()
+                    });
+
+                }
+                reader1.Close();
             }
-
-            reader1.Close();
 
             //Get the order products
             var sql2 =
                 "SELECT op.price, op.order_id, op.product_id, op.quantity, p.name, p.price FROM orderproduct op INNER JOIN product p on op.product_id=p.product_id";
 
-            var reader2 = database.ExecuteReader(sql2);
-
-            var values2 = new List<OrderProduct>();
-
-            while (reader2.Read())
-            {
-                var record2 = (IDataRecord)reader2;
-
-                values2.Add(new OrderProduct()
+            //HS - Using statement will take care of  disposing
+            using (var reader2 = database.ExecuteReader(sql2))
+            { 
+                while (reader2.Read())
                 {
-                    OrderId = record2.GetInt32(1),
-                    ProductId = record2.GetInt32(2),
-                    Price = record2.GetDecimal(0),
-                    Quantity = record2.GetInt32(3),
-                    Product = new Product()
-                    {
-                        Name = record2.GetString(4),
-                        Price = record2.GetDecimal(5)
-                    }
-                });
-             }
+                    var record2 = (IDataRecord)reader2;
 
-            reader2.Close();
+                    values2.Add(new OrderProduct()
+                    {
+                        OrderId = record2.GetInt32(1),
+                        ProductId = record2.GetInt32(2),
+                        Price = record2.GetDecimal(0),
+                        Quantity = record2.GetInt32(3),
+                        Product = new Product()
+                        {
+                            Name = record2.GetString(4),
+                            Price = record2.GetDecimal(5)
+                        }
+                    });
+                }
+
+                reader2.Close();
+            }
 
             foreach (var order in values)
             {
-                foreach (var orderproduct in values2)
+                //use LINQ to find exact matches instead of looping whole List
+                foreach (var orderproduct in values2.Where(op=>op.OrderId == order.OrderId))
                 {
-                    if (orderproduct.OrderId != order.OrderId)
-                        continue;
-
                     order.OrderProducts.Add(orderproduct);
                     order.OrderTotal = order.OrderTotal + (orderproduct.Price * orderproduct.Quantity);
                 }
